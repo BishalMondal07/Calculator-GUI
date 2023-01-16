de64155de8513be9d2b53daac57298cdbaddb24c
Bishal Mondal <89398209+BishalMondal07@users.noreply.github.com>
2023-01-16 23:09:05 +0530

    Add files via upload

---
+++ Basic calculator.py
@@ -0,0 +1,12 @@
+#write the inputs in seperate lines in proper sequence 
+num1 = int(input())
+num2 = int(input())
+operation = input()
+if operation == "+" :
+    print(num1 + num2)
+elif operation == "-" :
+    print(num1 + num2 )
+elif operation == "/" :
+    print(num1/num2)
+elif operation == "*" :
+    print(num1*num2)
