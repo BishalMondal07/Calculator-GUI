24c5595889f2d3263713881adbcc139e34661389
Bishal Mondal <89398209+BishalMondal07@users.noreply.github.com>
2023-01-19 11:56:20 +0530

    Update Basic calculator.py

--- Basic calculator.py
+++ Basic calculator.py
@@ -1,7 +1,7 @@
 #write the inputs in seperate lines in proper sequence 
-num1 = int(input())
-num2 = int(input())
-operation = input()
+num1 = int(input("Enter the number :"))
+num2 = int(input("Enter the number :"))
+operation = input("Operation :")
 if operation == "+" :
     print(num1 + num2)
 elif operation == "-" :
